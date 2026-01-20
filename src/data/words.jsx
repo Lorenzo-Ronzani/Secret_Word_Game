9244eb18551c2396695a34c6baeb355d6cb17b8b
Lorenzo Ronzani <lorenzo.lronzani@gmail.com>
2026-01-19 18:13:58 -0700

    Initial commit

---
+++ src/data/words.jsx
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Banco', 'Pneu', 'Antena'],
+    fruta: ['Banana', 'Morango', 'Uva', 'Abacate', 'Laranja'],
+    corpo: ['Braço', 'Perna', 'Cérebro', 'Pescoço', 'Olhos'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete'],
+    programação: ['Linguagem', 'Framework', 'JavaScript', 'React'],
+    alimento: ['Arroz', 'Alface', 'Carne', 'Leite', 'Ovo'],
+};
